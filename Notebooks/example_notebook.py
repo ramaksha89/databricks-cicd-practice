c8ce5927fe8c8fce6847934cfe0c46e4466d0891
ramaksha89 <ramu@SEETHARAMs-MacBook-Pro.local>
2025-08-27 00:47:07 -0500

    Add Github Actions to deploy notebooks to Databricks

---
+++ Notebooks/example_notebook.py
@@ -0,0 +1,15 @@
+# example_notebook.py (exported version) #new comment 
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+# Batch example: Load sample CSV
+df = spark.read.csv("/databricks-datasets/learning-spark-v2/people/people-10m.csv", header=True, inferSchema=True)
+
+df_transformed = df.filter(df.age > 30).select("name", "age")
+df_transformed.show(5)
+
+# Streaming example (later extension)
+# stream_df = spark.readStream.format("rate").option("rowsPerSecond", 10).load()
+# transformed = stream_df.withColumnRenamed("value", "my_value")
+# query = transformed.writeStream.format("console").start()
